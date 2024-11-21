69556e32362ddb662bd2561b86371fc164565b52
akshayrajeevnambiar <akshayrajeevnambiar@gmail.com>
2024-11-20 19:51:34 -0500

    challenge #2

---
+++ palindrome_checker.py
@@ -0,0 +1,6 @@
+# Python Code Challenge #2: Identify a Palindrome
+# -----------------------------------------------
+# Your goal is to implement a function, is_palindrome(), that takes a text string as the input argument and returns a boolean indicating whether or not it's a palindrome.
+
+def is_palindrome(value):
+    pass
