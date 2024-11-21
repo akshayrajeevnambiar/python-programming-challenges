584b16d712d8e6a2a79302654e291b09fa41d792
akshayrajeevnambiar <akshayrajeevnambiar@gmail.com>
2024-11-20 20:17:59 -0500

    solution

--- sorting_strings.py
+++ sorting_strings.py
@@ -2,5 +2,11 @@
 # ----------------------------------------
 # Your goal is to implement a function, sort_words(), that takes a string containing one or more words separated by spaces as the input argument and returns a string containing those words sorted alphabetically.
 
+import re
+
 def sort_words(phrase):
-    pass
+    words = re.findall(r'[a-zA-Z]+', phrase)
+
+    return ' '.join(sorted(words, key= lambda s: s.casefold()))
+
+print(sort_words("banana ORANGE apple"))
