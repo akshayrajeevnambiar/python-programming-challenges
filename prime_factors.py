34bbfc4b6fe5f375aca8e62c6c2a93bd7b7bccf8
akshayrajeevnambiar <akshayrajeevnambiar@gmail.com>
2024-11-20 19:20:53 -0500

    prime factors challenge

---
+++ prime_factors.py
@@ -0,0 +1,6 @@
+# Python Code Challenge #1: Find Prime Factors
+# ----------------------------------------------
+# Your goal is to implement a function, get_prime_factors(), that takes an integer value as the input argument and returns a list containing all of its prime factors.
+
+def get_prime_factors(value):
+    pass
